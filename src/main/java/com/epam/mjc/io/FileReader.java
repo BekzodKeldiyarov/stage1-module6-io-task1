e53657b2fdfc4e5c161dc49b7d9fb155d855c070
bekzodkeldiyarov <bek.bek.bek.bek72@gmail.com>
2023-02-15 01:17:16 +0500

    Fix bugs

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,12 +1,17 @@
 package com.epam.mjc.io;
 
 import java.io.*;
-import java.sql.SQLOutput;
 import java.util.HashMap;
 import java.util.Map;
 
 
 public class FileReader {
+    public static void main(String[] args) {
+        FileReader reader = new FileReader();
+        Profile dataFromFile = reader.getDataFromFile(new File("/Users/mac/Desktop/code/stage1-module6-io-task1/src/main/resources/Profile.txt"));
+        System.out.println(dataFromFile);
+    }
+
     public Profile getDataFromFile(File file) {
         Profile profile = null;
         try (InputStream inputStream =
@@ -22,6 +27,8 @@ public class FileReader {
 
             profile = new Profile(data.get("name"), Integer.parseInt(data.get("age")), data.get("email"), Long.parseLong(data.get("phone")));
             return profile;
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
