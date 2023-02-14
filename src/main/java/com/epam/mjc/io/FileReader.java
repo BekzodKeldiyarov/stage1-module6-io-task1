2ed03bef3dff32bf75742139fff74685b1e501c8
bekzodkeldiyarov <bek.bek.bek.bek72@gmail.com>
2023-02-15 01:10:20 +0500

    Done

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,35 @@
 package com.epam.mjc.io;
 
-import java.io.File;
+import java.io.*;
+import java.sql.SQLOutput;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class FileReader {
+    public static void main(String[] args) {
+        FileReader reader = new FileReader();
+        Profile dataFromFile = reader.getDataFromFile(new File("/Users/mac/Desktop/code/stage1-module6-io-task1/src/main/resources/Profile.txt"));
+        System.out.println(dataFromFile);
+    }
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        Profile profile = null;
+        try (InputStream inputStream =
+                     new FileInputStream(file)) {
+            byte[] bytes = inputStream.readAllBytes();
+            String s = new String(bytes);
+            Map<String, String> data = new HashMap<>();
+            String[] line = s.split("\n");
+            for (String i : line) {
+                String[] keyValue = i.split(": ");
+                data.put(keyValue[0].toLowerCase(), keyValue[1]);
+            }
+
+            profile = new Profile(data.get("name"), Integer.parseInt(data.get("age")), data.get("email"), Long.parseLong(data.get("phone")));
+            return profile;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
